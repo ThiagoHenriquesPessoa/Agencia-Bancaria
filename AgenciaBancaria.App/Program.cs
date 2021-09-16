f79af62e87b2b2b49d910594e756d5a27ee862cd
Thiago Henriques Pessoa <thiagokxl5@gmail.com>
2021-09-16 08:35:42 -0300

    Criação do projeto, dominio e app

---
+++ AgenciaBancaria.App/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AgenciaBancaria.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
